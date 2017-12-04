48cc51c6d199d03eb9238583c51bcf189cd72989
wuyunfeng <1363321437@qq.com>
2017-12-04 17:33:41 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,9 +10,9 @@
         </div>
     </div>
     <div class="ui container server">
-        <div class="header">
+        <h3 class="header">
             我们的服务
-        </div>
+        </h3>
         <div class="sub_header">
             无论您是哪个组织、企业还是个人、小商铺这里都能为您的需求找到答案
         </div>
@@ -62,7 +62,51 @@
         <button type="button" name="button" class="ui button blue">更多服务</button>
     </div>
     <div class="ui container solution">
-        
+        <h3 class="header">打破行业局限的解决方案，满足广泛的业务需求</h3>
+        <div class="sub_title">电商、零售、餐饮、医疗、消防、国电、休闲娱乐、大数据云平台，优燃佳都有完整解决方案</div>
+        <div class="ui grid">
+            <div class="doubling stackable four column row">
+                <div class="column">
+                    <i class="fa fa-cubes" aria-hidden="true"></i>
+                    <header>终端硬件</header>
+                    <p>
+                        智能硬件终端的一站式采购，支付pos终端、数据采集终端、wifi探测模块、大数据展示大屏、云平台指挥大屏、员工定制操作终端、厂区感应终端等
+                    </p>
+                </div>
+                <div class="column">
+                    <header>服务硬件</header>
+                </div>
+                <div class="column">
+                    <header>技术产品</header>
+                </div>
+                <div class="column">
+                    <header>广告推广</header>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ui blackserve">
+        <h3 class="header">强大的研发实力，稳定高效的技术服务</h3>
+        <div class="sub_title">年轻且顶尖的技术研发团队，打造99.99%高可用系统</div>
+        <div class="centent">
+            <div class="image">
+                <img src="../assets/images/l6_img1.png" alt="">
+            </div>
+            <div class="list-box">
+                <div class="three-box">
+                    <div class="box">
+                        <p>99.99 <span>%</span></p>
+                        <p class="sub">系统可用</p>
+                    </div>
+                    <div class="box">
+                        123
+                    </div>
+                    <div class="box">
+                        123
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </template>
