22b414f0463ee1bb28f29a1640ca3dd94581a213
wuyunfeng <wuyunfeng@wuyunfengdeiMac.local>
2018-10-05 13:15:20 +0800

    update

--- src/components/pageFooter.vue
+++ src/components/pageFooter.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="ui footer">
-        <div class="content">
+        <!-- <div class="content">
 
-        </div>
+        </div> -->
         <div class="footer">
             <p>
-                @武汉优燃佳科技有限公司
+                武汉优燃佳科技有限公司 © 2018 Implement By UranPlus
             </p>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-  name: "pageFooter",
-  data() {
-    return {
-      msg: "页脚啊"
+    export default {
+        name: "pageFooter",
+        data() {
+            return {
+                msg: "页脚啊"
+            };
+        }
     };
-  }
-};
 </script>
--- src/components/pageNavBar.vue
+++ src/components/pageNavBar.vue
@@ -1,38 +1,38 @@
 <template>
-<div class="ui navbar">
-    <div class="ui container">
-        <div class="content">
-            <a href="#" class="header">
-                <img src="../assets/images/logo.png" alt="logo">
-                <span>优燃佳</span>
-            </a>
-            <ul id="nav-mobile" class="menu">
-                <li>
-                    <router-link to="/">首页</router-link>
-                </li>
-                <li>
-                    <router-link to="/about">关于我们</router-link>
-                </li>
-                <li>
-                    <router-link to="/service">技术服务</router-link>
-                </li>
-                <li>
-                    <router-link to="/service">产品方案</router-link>
-                </li>
-            </ul>
-            <button type="button" name="button" class="ui blue button">预约我们</button>
+    <div class="ui navbar">
+        <div class="ui container">
+            <div class="content">
+                <a href="#" class="header">
+                    <img src="../assets/images/logo.png" alt="logo">
+                    <span>UranPlus</span>
+                </a>
+                <ul id="nav-mobile" class="menu">
+                    <li>
+                        <router-link to="/">首页</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/1">关于我们</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/1">技术服务</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/1">产品方案</router-link>
+                    </li>
+                </ul>
+                <button type="button" name="button" class="ui blue button">联系我们</button>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
-export default {
-  name: "pageNavBar",
-  data() {
-    return {
-      prdId: parseInt(Math.random())
+    export default {
+        name: "pageNavBar",
+        data() {
+            return {
+                prdId: parseInt(Math.random())
+            };
+        }
     };
-  }
-};
 </script>
